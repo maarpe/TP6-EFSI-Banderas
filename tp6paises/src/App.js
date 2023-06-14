7f5f8e2506cbc4ff30198b625685e2ebd1192325
MARTIN ARIEL PEREZ <47026770@ort>
2023-06-14 09:03:56 -0300

    hacer que funcione el boton

--- tp6paises/src/App.js
+++ tp6paises/src/App.js
@@ -3,45 +3,55 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const[ contador, setContador] = useState(0);
-  const [ nombre, setNombre ] = useState('');
-  const [Pais, setPais] = useState('');
-  const [randomPais, setRandom] = useState(0);
+  const [contador, setContador] = useState(0);
+  const [nombre, setNombre] = useState('');
+  const [paises, setPaises] = useState([]);
+  const [randomPais, setRandom] = useState({});
 
   useEffect(() => {
     axios.get('https://countriesnow.space/api/v0.1/countries/flag/images')
       .then((response) => {
-        setPais(response.data.data);
-        console.log(response);
+        const paisesResponse = response.data.data;
+        setPaises(paisesResponse);
+
+        const randomPais = paisesResponse[Math.floor(Math.random() * paisesResponse.length)];
+        setRandom(randomPais);
       });
+    
+  }, []);
 
+  let cambiarPais = () => {
+    let id = Math.floor(Math.random() * (paises.length + 1));
+    setPaises(paises[id]);
+  }
 
-      setRandom(Math.floor(Math.random() * 220));
-      console.log(randomPais);
+  useEffect(() => {
+    if(nombre === randomPais.name){
+      contador = contador + 10;
+      cambiarPais();
+    }
+    else{
+      if(contador > 0) setContador(contador - 1);
+    }
+  })
   
-  
-    },[]);
+  return (
+    <div className='App-header'>
+      <div className='container'>
+        {randomPais.flag ? <img src={randomPais.flag} /> : <div></div>}
+        <div className='botonSubmit'>
+          <input type="text" class="form-control" placeholder="Nombre bandera" onSubmit={(e) => setNombre(e.target.value)} />
+          <button onSubmit={contador}>Ver si es correcto</button>
+          <div className='contador'>
+            PUNTOS: {contador}
+          </div>
+        </div>
 
-  return(
-    <div>
-      {nombre} <br></br> 
-     <input type="text" class="form-control" placeholder="Nombre bandera" onKeyUp={(e) => setNombre(e.target.value)} />
-     
-      <button onSubmit={contador}>Ver si es correcto</button>
 
-
+      </div>
     </div>
 
-
   );
-
-
-
-
-
-
-
-
 }
 
 export default App;
